bd2eea571b67e9598e4332074e830b97f03ecec2
Dmitrijs Balcers <dmitrijs.balcers@d8corp.com>
2015-04-14 21:11:45 +0300

    First commit

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+var expect = require("chai").expect;
+
+describe("JSON", function () {
+    describe(".parse()", function () {
+        it("should detect malformed JSON strings", function () {
+            expect(4 + 6).to.equal(9);
+        });
+    });
+});
