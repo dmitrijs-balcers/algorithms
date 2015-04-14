fc9fd510da7fefcfdf03b642f7de64eecf570ef7
Dmitrijs Balcers <dmitrijs.balcers@d8corp.com>
2015-04-14 22:05:11 +0300

    Add union and check connection methods

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,46 @@
+// http://code.tutsplus.com/tutorials/testing-in-nodejs--net-35018
+
 var expect = require("chai").expect;
 
-describe("JSON", function () {
+function UF(arr) {
+    this.arr = arr;
+}
+
+UF.prototype.connected = function (p, q) {
+    return this.arr[p] === this.arr[q];
+};
+
+UF.prototype.union = function (p, q) {
+    var arr = this.arr, pid = arr[p], qid = arr[q];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === pid) (arr[i] = qid)
+    }
+    return this;
+};
+
+
+describe("Union Find", function () {
     describe(".parse()", function () {
-        it("should detect malformed JSON strings", function () {
-            expect(4 + 6).to.equal(9);
+        it("should connect two paths", function () {
+            var uf = new UF([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            uf.union(1, 2);
+
+            expect(uf.arr).to.eql([0, 2, 2, 3, 4, 5, 6, 7, 8, 9]);
+        });
+        it("should find connection between two paths", function () {
+            var uf = new UF([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            uf.union(1, 2);
+
+            expect(uf.connected(1, 2)).to.be.true;
+        });
+
+        it("should connect three paths", function () {
+            var uf = new UF([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            uf.union(1, 2);
+            uf.union(2, 3);
+
+            expect(uf.arr).to.eql([0, 3, 3, 3, 4, 5, 6, 7, 8, 9]);
         });
     });
 });
