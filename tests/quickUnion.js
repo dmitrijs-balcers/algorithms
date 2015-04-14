8c318446fb91271674398f1bb5b13efabf81f7dc
Dmitrijs Balcers <dmitrijs.balcers@d8corp.com>
2015-04-14 22:57:55 +0300

    Fix bug in find function of quick union example

--- tests/quickUnion.js
+++ tests/quickUnion.js
@@ -6,7 +6,12 @@ function QU(arr) {
 
 QU.prototype.find = function (p, q) {
     var arr = this.arr;
-    return arr[p] === arr[q];
+
+    return _root(p) === _root(q);
+
+    function _root(i) {
+        return i !== arr[i] ? _root(arr[i]) : i;
+    }
 
 };
 
@@ -42,5 +47,14 @@ describe("Quick Union", function () {
         uf.union(4, 3).union(5, 3);
 
         expect(uf.find(4, 7)).to.be.false;
+    });
+
+    it("should find connection between several paths", function () {
+        var uf = new QU([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        uf.union(4, 3).union(5, 3).union(9,5);
+
+        console.log(uf.arr);
+
+        expect(uf.find(4, 9)).to.be.true;
     });
 });
