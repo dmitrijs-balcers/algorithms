8626f8ea8263eaf88396da71a0be57091d8cb57f
Dmitrijs Balcers <dmitrijs.balcers@d8corp.com>
2015-04-14 23:14:30 +0300

    Improve performance and add test

--- tests/quickUnion.js
+++ tests/quickUnion.js
@@ -1,22 +1,24 @@
+// Trees can get too tall.
+// Find is too expensive
+
 var expect = require("chai").expect;
 
 function QU(arr) {
-    this.arr = arr;
+    this.arr = arr; // Order of growth N
 }
 
-QU.prototype.find = function (p, q) {
-    var arr = this.arr;
-
-    return _root(p) === _root(q);
-
-    function _root(i) {
-        return i !== arr[i] ? _root(arr[i]) : i;
-    }
-
+QU.prototype.root = function (i) {
+    return i !== this.arr[i] ? this.root(this.arr[i]) : i;
 };
 
-QU.prototype.union = function (p, q) {
-    this.arr[p] = q;
+QU.prototype.find = function (child, parent) {
+    return this.root(child) === this.root(parent); // Order of growth === N
+};
+
+QU.prototype.union = function (child, parent) {
+    // It is required to find ar root, so union of trees would be performed at roots
+    // In such way, trees will be less deep
+    this.arr[this.root(child)] = this.root(parent); // Order of growth N (includes cost of finding roots)
     return this;
 };
 
@@ -53,8 +55,15 @@ describe("Quick Union", function () {
         var uf = new QU([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
         uf.union(4, 3).union(5, 3).union(9,5);
 
-        console.log(uf.arr);
-
         expect(uf.find(4, 9)).to.be.true;
+    });
+    
+    it("should union two trees", function () {
+        var uf = new QU([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        uf.union(0, 1).union(2, 1).union(3,2);
+        uf.union(4, 5).union(6, 5).union(7,4);
+        uf.union(0, 7);
+
+        expect(uf.find(0, 7)).to.be.true;
     });
 });
